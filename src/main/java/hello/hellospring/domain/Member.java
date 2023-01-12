7b926ee89f95fdc8e035dc2dfd624e370977d31b
jihyun <cat2998@naver.com>
2023-01-13 00:50:15 +0900

    0112 spring-basic

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 알아서 id를 생성하는것을 identity전략이라고 한다
     private Long id;
+
     private String name;
 
     public Long getId() {
